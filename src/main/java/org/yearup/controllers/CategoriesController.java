adc4660d9e6d90e4ed0592ab35687dcd6ce3970c
daliseb <odaliseb59@gmail.com>
2025-12-16 10:08:07 -0500

    trying to fix category controller

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,12 +1,16 @@
 package org.yearup.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
 import org.yearup.models.Product;
 
+import java.math.BigDecimal;
 import java.util.List;
 //to make this a REST controller
 @RestController
@@ -21,11 +25,31 @@ public class CategoriesController {
 
     @Autowired
     public CategoriesController(CategoryDao categoryDao){
+
         this.categoryDao = categoryDao;
 }
     //^^^create an Autowired controller to inject the categoryDao and ProductDao
 
-    // add the appropriate annotation for a get action
+    @GetMapping("")
+    @PreAuthorize("permitAll()")
+    public List<Category> search(@RequestParam(name="cat", required = false) Integer categoryId,
+                                 @RequestParam(name="minPrice", required = false) BigDecimal minPrice,
+                                 @RequestParam(name="maxPrice", required = false) BigDecimal maxPrice,
+                                 @RequestParam(name="subCategory", required = false) String subCategory
+                                )
+    {
+        try
+        {
+            return categoryDao.search(categoryId, minPrice, maxPrice, subCategory);
+        }
+        catch(Exception ex)
+        {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+        }
+
+        }
+
+    // ^^add the appropriate annotation for a get action
     public List<Category> getAll()
     {
         // find and return all categories
