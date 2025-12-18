1515983b3a1fdad4c2d36fde682e0668530f1747
daliseb <odaliseb59@gmail.com>
2025-12-18 03:00:38 -0500

    override for create

--- src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -5,10 +5,7 @@ import org.yearup.data.CategoryDao;
 import org.yearup.models.Category;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -72,7 +69,31 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
     @Override
     public Category create(Category category)
     {
-        // create a new category
+        String sql = "INSERT INTO categories (name, description) VALUES (?,?)";
+                try (Connection connection = getConnection())
+        {
+            PreparedStatement statement =
+                    connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, category.getName());
+            statement.setString(2, category.getDescription());
+
+            int rowsAffected = statement.executeUpdate();
+
+            if (rowsAffected > 0 )
+            {
+                ResultSet keys = statement.getGeneratedKeys();
+                if (keys.next())
+                {
+                    int categoryId = keys.getInt(1);
+                    return getById(categoryId);
+                }
+            }
+        }
+                catch(SQLException e )
+                {
+                    throw new RuntimeException(e);
+                }
+                return null;
     }
 
     @Override
